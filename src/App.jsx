74c9c7960e47adbafae5355937d688028ffc1249
Luka Talakvadze <lukatalakvadze@gmail.com>
2024-03-15 13:39:34 +0400

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="w-full flex flex-col items-center">
+        <img
+          className="absolute z-[-1] top-0 left-0 w-full"
+          src="/public/assets/images/mobile/first-circle-mobile.svg"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
